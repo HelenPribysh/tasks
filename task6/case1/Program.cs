783817f22d3e5b6609e8d6e3b6d57de63b4cbbec
HelenPribysh <helenpribysh@gmail.com>
2019-03-24 20:37:07 +0300

    hm6 implemented

---
+++ task6/case1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace case1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            string str = Console.ReadLine();
+            str = str.Replace('о', 'а').Replace('О', 'А').Replace('o', 'a').Replace('O', 'A');
+            string[] separatedString = str.Split(';');
+            
+            foreach (string i in separatedString)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
