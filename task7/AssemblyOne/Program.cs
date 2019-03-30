7c2a73c6147847e551e54bbd2caceb15a9cbdf32
HelenPribysh <helenpribysh@gmail.com>
2019-03-30 13:16:53 +0300

    Task7 implemented

---
+++ task7/AssemblyOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AssemblyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Employee.CompanyNamePublic;
+            string b = Employee.CompanyNameInternal;
+            string d = Employee.CompanyNamePprotectedInternal;
+
+            string a1 = Employee2.CompanyNamePublic;
+            string b1 = Employee2.CompanyNameInternal;
+            string d1 = Employee2.CompanyNamePprotectedInternal;
+
+            //other fields can't be got access
+        }
+    }
+}
---
+++ task7/AssemblyTwo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AssemblyOne;
+
+namespace AssemblyTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a1 = Employee2.CompanyNamePublic;
+
+            //other fields can't be got access
+        }
+    }
+}
