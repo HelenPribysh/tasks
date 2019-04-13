61612fce0d8b11a7e100daac122c51f5ecfcdf68
HelenPribysh <helenpribysh@gmail.com>
2019-04-13 21:06:40 +0300

    Uploaded task_9

---
+++ task_9/task_9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static task9.Genres;
+using Newtonsoft.Json;
+
+namespace task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song = new Song("Umbrella", 4, "Rihanna", 2007, pop);            
+            var data = GetSongData(song);
+
+            Console.WriteLine(data);
+            JsonConvert.SerializeObject(data);
+        }
+
+        public static dynamic GetSongData(Song song)
+        {
+            var data = new
+            {
+                Name = song.NameOfSong,
+                Lenth = song.LengthOfSong,
+                Year = song.DataOfRelease
+            };
+
+            //Console.WriteLine($"{song.NameOfSong}, {song.LengthOfSong}, {song.DataOfRelease}");
+            return data;
+        }
+    }
+}
---
+++ task_9/task_9/Song.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace task9
+{
+    class Song
+    {
+        public string NameOfSong { get => NameOfSong; set => NameOfSong = value; }
+        public double LengthOfSong { get => LengthOfSong; set => LengthOfSong = value; }
+        public string Author { get => Author; set => Author = value; }
+        public int DataOfRelease { get => DataOfRelease; set => DataOfRelease = value; }
+        internal Genres Genres { get => Genres; set => Genres = value; }
+
+        public Song(string _nameOfSong, double _lengthOfSong, string _author, int _dataOfRelease, Genres _genres)
+        {
+            NameOfSong = _nameOfSong;
+            LengthOfSong = _lengthOfSong;
+            Author = _author;
+            DataOfRelease = _dataOfRelease;
+            Genres = _genres;
+        }
+
+        
+    }
+
+    internal enum Genres
+    {
+        pop, metal, reggi
+    };
+
+}
