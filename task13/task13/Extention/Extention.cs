5716cbb58cd26b747ff47472cce8f24945a913e2
HelenPribysh <helenpribysh@gmail.com>
2019-04-26 23:40:21 +0300

    Implemented task13

---
+++ task13/task13/Extention/Extention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task13.Extention
+{
+    public static class Extention
+    {
+        public static int GetFreeCapacity(this Stack stack)
+        {
+            return 10 - stack.Count;
+        }
+
+        public static int GetFreeCapacity<T>(this StackT <T> stackT)
+        {
+            return 10 - stackT.Count;
+        }
+    }
+}
---
+++ task13/task13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using task13.Extention;
+
+namespace task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            StackT<char> stackT = new StackT<char>();
+
+            stack.Push(2);
+            stack.Push(3);
+            stack.Pop();
+            Console.WriteLine(stack.GetFreeCapacity());
+            Console.WriteLine(stack.ToString());
+            Console.WriteLine(stack.Peek());
+
+            stackT.Push('a');
+            stackT.Push('b');
+            stackT.Pop();
+            Console.WriteLine(stackT.GetFreeCapacity());
+            Console.WriteLine(stackT.ToString());
+            Console.WriteLine(stackT.Peek());
+        }
+    }
+}
